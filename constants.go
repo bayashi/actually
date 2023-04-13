8ea69b2cbbdb51bf677ac6e5c2537f3d6764ec72
bayashi <bayashi@cpan.org>
2023-04-13 10:17:22 +0000

    Add `SamePointer()` method

--- constants.go
+++ constants.go
@@ -3,8 +3,11 @@ package actually
 const (
 	TraceSeparator = "\n\t"
 
-	FailReason_WrongType    = "Wrong Type"
-	FailReason_NotSame      = "Not same"
-	FailReason_GotIsFunc    = "`Got` value is type of function. It cannot be used in Same() method"
-	FailReason_ExpectIsFunc = "`Expect` value is type of function. It cannot be used in Same() method"
+	FailReason_WrongType           = "Wrong Type"
+	FailReason_NotSame             = "Not same"
+	FailReason_WrongPointerAddress = "Wrong Type"
+	FailReason_GotIsFunc           = "`Got` value is type of function. It cannot be used in Same() method"
+	FailReason_ExpectIsFunc        = "`Expect` value is type of function. It cannot be used in Same() method"
+	FailReason_GotIsNotPointer     = "`Got` is NOT type of Pointer. It should be a Pointer for SamePointer() method"
+	FailReason_ExpectIsNotPointer  = "`Expect` is NOT type of Pointer. It should be a Pointer for SamePointer() method"
 )
--- same.go
+++ same.go
@@ -6,7 +6,8 @@ import (
 )
 
 // `Same` verifies that two objects are same in value and type.
-// Function is not acceptable. Not verify pointer address.
+// Function type value is not acceptable. And not verify pointer address.
+// It will be fail, int(1) and uint(1), because of type.
 func (a *testingA) Same(t *testing.T) *testingA {
 	a.t = t
 
@@ -27,6 +28,32 @@ func (a *testingA) Same(t *testing.T) *testingA {
 	if !objectsAreSame(a.expect, a.got) {
 		a.t.Helper()
 		return a.fail(reportForSame(a).Reason(FailReason_NotSame))
+	}
+
+	return a
+}
+
+// `SamePointer` verifies that two objects point to the same object.
+func (a *testingA) SamePointer(t *testing.T) *testingA {
+	a.t = t
+
+	if !isPointerType(a.got) {
+		a.t.Helper()
+		return a.fail(reportForSame(a).Reason(FailReason_GotIsNotPointer))
+	}
+	if !isPointerType(a.expect) {
+		a.t.Helper()
+		return a.fail(reportForSame(a).Reason(FailReason_ExpectIsNotPointer))
+	}
+
+	if reflect.TypeOf(a.got) != reflect.TypeOf(a.expect) {
+		a.t.Helper()
+		return a.fail(reportForSame(a).Reason(FailReason_WrongType))
+	}
+
+	if a.got != a.expect {
+		a.t.Helper()
+		return a.fail(reportForSame(a).Reason(FailReason_WrongPointerAddress))
 	}
 
 	return a
